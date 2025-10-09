b16b2dbd6134ccb40dc5b80f9b145da94c063ab6
Fer <desarrollowebfer@gmail.com>
2025-10-09 09:54:44 +0200

    PPB-10-fe-iniciar-sesion-y-persistencia-de-sesion feat: create folder styles and files Variables.css and Base.css

--- src/pages/Loader.jsx
+++ src/pages/Loader.jsx
@@ -2,20 +2,25 @@ import React from "react";
 
 import "./Loader.css";
 
-const Main = () => {
+const Loader = () => {
 
     return (
-        <main>
+        <main className="main">
+            {/*Section loader*/}
+            <section className="main_loader">
+                <div className="main_loader_loadingPie">
+                    <div className="main_loader_logo">
+                        <p className="main_loader_logo-text">Fila-Zero</p>
+                    </div>
+                    <div className="main_loading-content ">
+                        <p className="main_loading-text">Cargando tus empanadas...</p>
+                    </div>
+                </div>
 
-
+            </section>
         </main>
     )
 
-
-
-
-
-
 }
 
-export default Main;
+export default Loader;
