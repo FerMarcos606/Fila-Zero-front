7556b4d2afc5a4f6c67e34e94ed35255bf294091
Fer <desarrollowebfer@gmail.com>
2025-10-09 11:41:37 +0200

    PPB-10-fe-iniciar-sesion-y-persistencia-de-sesion feat: finished initial page syles and code

--- src/pages/Loader.jsx
+++ src/pages/Loader.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import empanadaIcon from '../assets/imgs/empanada.svg';
 
 import "./Loader.css";
 
@@ -8,14 +9,25 @@ const Loader = () => {
         <main className="main">
             {/*Section loader*/}
             <section className="main_loader">
-                <div className="main_loader_loadingPie">
-                    <div className="main_loader_logo">
-                        <p className="main_loader_logo-text">Fila-Zero</p>
+                    <div className="main_spinner">
+                        {/* Este div container spinner and logo, 
+                            one over the other */ }
+                        <div className="spinner-container"> 
+                            {/* circle without logo */}
+                            <div className="main_loader_loadingPie">
+                            {/* Spinner fade */}
+                            </div>
+                            
+                            {/* Logo must be fixed over spinner*/}
+                            <div className="main_loader_logo">
+                            <img src={empanadaIcon} alt="icono círculo en fondo blanco con borde amarillo" />
+                            <p className="main_loader_logo-text">Fila-Zero</p>
+                            </div>
+                        </div>
+                        <div className="main_loading-content ">
+                            <h2 className="main_loading-text">Cargando tus empanadas...</h2>
+                        </div>
                     </div>
-                    <div className="main_loading-content ">
-                        <p className="main_loading-text">Cargando tus empanadas...</p>
-                    </div>
-                </div>
 
             </section>
         </main>
